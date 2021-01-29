c403d7a7a9153708680bcf28d5defd94cad89edc
Allie <alliecaton3@gmail.com>
2021-01-29 17:28:23 -0500

    commit tree 242db5e1f50c3c3414a20b5de67af4925c095e20

--- config.ru
+++ config.ru
@@ -1,9 +1,15 @@
 require './config/environment'
+# require_relative 'app/controllers/artists_controller'
+# require_relative 'app/controllers/genres_controller'
+# require_relative 'app/controllers/songs_controller'
 
 begin
   fi_check_migration
 
   use Rack::MethodOverride
+  use ArtistsController
+  use GenresController
+  use SongsController
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,25 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+# gec = Artist.create(name: "100 gecs")
+# gec.songs << Song.create(name: "Money Machine")
+
+# charli = Artist.create(name: "Charlie XCX")
+# charli.songs << Song.create(name: "Detonate")
+# charli.songs << Song.create(name: "Vroom Vroom")
+
+# pop = Genre.create(name: "pop")
+# hyperpop= Genre.create(name: "hyperpop")
+# electronic = Genre.create(name: "electronic")
+
+# money_machine = Song.find_by(name: "Money Machine")
+# money_machine.genre = hyperpop
+
+# detonate = Song.find_by(name: "Detonate")
+# detonate.genres = pop
+
+# vroom = Song.find_by(name: "Vroom Vroom")
+# vroom.genre = electronic
+
+LibraryParser.parse
+
+
